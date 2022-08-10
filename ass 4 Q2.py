f38699f86e3c0d49d6ecedc2296624fce5d63957
Danesh Kundaragi <109088091+danesh33@users.noreply.github.com>
2022-08-10 12:58:17 +0530

    Add files via upload

---
+++ ass 4 Q2.py
@@ -0,0 +1,3 @@
+a = (1, 2, 3, 4, 5, 6, 7) 
+result = map(lambda x: x + x + x, a) 
+print(list(result))
---
+++ ass 4 Q3.py
@@ -0,0 +1,6 @@
+def square_num(n):
+          return n * n
+a = [4, 5, 2, 9]
+result = map(square_num, a)
+
+print(list(result))
